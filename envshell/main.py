583e019cdef59a77a42665f233d1e57de395b864
Enviction <schmidtjonas662@gmail.com>
2025-02-14 19:39:12 +0100

    added: EnvLight - A replacement for App Runners

--- envshell/main.py
+++ envshell/main.py
@@ -1,14 +1,18 @@
 from modules.envnotch.envnotch import EnvNotch
 from modules.envpanel.envpanel import EnvPanel
+from modules.envnoti.envnoti import EnvNoti
 from modules.envdock.envdock import EnvDock
+from modules.envlight.envlight import EnvLight
 from fabric import Application
 import loguru
 
 if __name__ == "__main__":
 	envnotch = EnvNotch()
 	envpanel = EnvPanel()
+	envnoti = EnvNoti()
 	envdock = EnvDock()
-	app = Application("envshell", envnotch, envdock, envpanel)
+	envlight = EnvLight()
+	app = Application("envshell", envnoti, envnotch, envdock, envpanel, envlight)
 
 	def set_css():
 		app.set_stylesheet_from_file(
--- envshell/modules/envlight/envlight.py
+++ envshell/modules/envlight/envlight.py
@@ -8,6 +8,7 @@ from fabric.widgets.button import Button
 from fabric.widgets.label import Label
 from fabric.widgets.svg import Svg
 from fabric.widgets.box import Box
+from fabric.widgets.entry import Entry
 from fabric.widgets.centerbox import CenterBox
 from fabric.widgets.wayland import WaylandWindow as Window
 from gi.repository import GLib
@@ -19,11 +20,13 @@ class EnvLight(Window):
 		super().__init__(
 			layer="overlay",
 			anchor="center center",
-			exclusivity="none",
+			exclusivity="auto",
 			name="env-light",
 			all_visible=True,
+			size=(600, 64),
 			visible=True,
-			margin=(0, 0, 0, 0),
+			keyboard_mode="exclusive",
+			pass_through=False,
 			**kwargs,
 		)
 
@@ -31,9 +34,33 @@ class EnvLight(Window):
 			name="light-content",
 			h_expand=True,
 			v_expand=True,
+			h_align="start",
+			orientation="v",
+			size=(600, 64),
 			children=[
+				Box (
+					name="light-header",
+					h_expand=True,
+					v_expand=True,
+					h_align="start",
+					orientation="h",
+					size=(600, 64),
+					children=[
+						Entry(placeholder="Envlight Search", name="light-entry"),
+					]
+				),
+				Label("Suggestion 1", name="light-suggestions-label", h_align="start"),
+				Label("Suggestion 2", name="light-suggestions-label", h_align="start"),
+				Label("Suggestion 3", name="light-suggestions-label", h_align="start"),
+				Label("Suggestion 4", name="light-suggestions-label", h_align="start"),
+				Label("Suggestion 5", name="light-suggestions-label", h_align="start"),
 			],
-			size=(200, 24),
 		)
 
-		self.children = []
+		# exit keybind
+		self.add_keybinding("Escape", lambda *_: self.hide())
+
+		self.children = [
+			self.light
+		]
+		self.show_all()
