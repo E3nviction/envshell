c6459ca59477fbebbc6147d7d6d64e9770e556f2
Enviction <schmidtjonas662@gmail.com>
2025-02-16 18:24:08 +0100

    refactor: envlight using the example on the fabric wiki

--- envshell/modules/envlight/envlight.py
+++ envshell/modules/envlight/envlight.py
@@ -1,125 +1,128 @@
-import threading
-import time
-import sys
-import os
-
+import operator
+from collections.abc import Iterator
 from fabric import Application
-from fabric.widgets.button import Button
-from fabric.widgets.label import Label
-from fabric.widgets.svg import Svg
 from fabric.widgets.box import Box
+from fabric.widgets.label import Label
+from fabric.widgets.button import Button
+from fabric.widgets.image import Image
 from fabric.widgets.entry import Entry
-from fabric.widgets.centerbox import CenterBox
+from fabric.widgets.scrolledwindow import ScrolledWindow
 from fabric.widgets.wayland import WaylandWindow as Window
-from gi.repository import GLib
+from fabric.utils import DesktopApp, get_desktop_applications, idle_add, remove_handler
 
-from config.c import c
-
-apps = {
-	"Brave Browser": "brave",
-	"Vesktop": "vesktop",
-	"Files": "nautilus",
-	"Settings": "XDG_CURRENT_DESKTOP=GNOME gnome-control-center",
-	"Software": "gnome-software",
-	"Disks": "gnome-disks",
-	"Calculator": "gnome-calculator",
-	"Clocks": "gnome-clocks",
-	"Terminal": "gnome-terminal",
-	"Maps": "gnome-maps",
-	"ChatGPT": "brave --app=https://chat.openai.com/",
-}
 
 class EnvLight(Window):
 	def __init__(self, **kwargs):
 		super().__init__(
-			layer="overlay",
-			anchor="center center",
-			exclusivity="auto",
-			name="env-light",
-			all_visible=False,
-			h_expand=True,
-			v_expand=True,
-			size=(600, 64),
-			visible=False,
+			layer="top",
+			anchor="center",
+			exclusivity="none",
 			keyboard_mode="on-demand",
-			pass_through=False,
+			name="env-light",
+			visible=False,
+			all_visible=False,
 			**kwargs,
 		)
+		self.add_keybinding("Escape", self.toggle)
+		self.set_property("width-request", 400)
+		self._arranger_handler: int = 0
+		self._all_apps = get_desktop_applications()
 
-		self.entry = Entry(placeholder="Envlight Search", name="light-entry", h_expand=True)
-
-		self.entry.connect("activate", self.submit)
-		self.entry.connect("changed", self.update_suggestions)
-
-		self.light = Box(
-			name="light-content",
+		self.viewport = Box(spacing=2, orientation="v")
+		self.search_entry = Entry(
+			placeholder="Search Applications...",
 			h_expand=True,
-			v_expand=True,
-			h_align="start",
-			orientation="v",
-			size=(600, 64),
-			children=[
-				Box (
-					name="light-header",
-					h_expand=True,
-					v_expand=True,
-					h_align="start",
-					orientation="h",
-					size=(600, 64),
-					children=[
-						self.entry,
-					]
-				),
-				Box(
-					name="light-suggestions",
-					h_expand=True,
-					v_expand=True,
-					h_align="start",
-					orientation="v",
-					size=(600, 64),
-				),
-			],
+			notify_text=lambda entry, *_: self.arrange_viewport(entry.get_text()),
+		)
+		self.scrolled_window = ScrolledWindow(
+			min_content_size=(280, 320),
+			max_content_size=(280 * 2, 320),
+			child=self.viewport,
 		)
 
-		# exit keybind
-		self.add_keybinding("Escape", lambda *_: self.hide())
-
-		self.children = [
-			self.light
-		]
-
-	def toggle(self, b):
+		self.add(
+			Box(
+				spacing=2,
+				orientation="v",
+				style="margin: 2px",
+				children=[
+					Box(
+						spacing=2,
+						orientation="h",
+						children=[
+							self.search_entry,
+						],
+					),
+					self.scrolled_window,
+				],
+			)
+		)
+	def toggle(self, b, *_):
 		if self.get_visible():
 			self.hide()
 		else:
-			self.show_light()
+			self.show_all()
 
-	def show_light(self):
-		self.entry.set_text("")
-		self.show_all()
+	def arrange_viewport(self, query: str = ""):
+		remove_handler(self._arranger_handler) if self._arranger_handler else None
 
-	def submit(self, b):
-		print(self.entry.get_text())
-		self.hide()
+		self.viewport.children = []
 
-	def update_suggestions(self, b):
-		# use apps
-		SUGGESTIONS = 5
-		text = self.entry.get_text()
-		fitting = []
+		filtered_apps_iter = iter(
+			[
+				app
+				for app in self._all_apps
+				if query.casefold()
+				in (
+					(app.display_name or "")
+					+ (" " + app.name + " ")
+					+ (app.generic_name or "")
+				).casefold()
+			]
+		)
+		should_resize = operator.length_hint(filtered_apps_iter) == len(self._all_apps)
 
-		for app in apps.items():
-			if str(app[0]).startswith(text):
-				fitting.append(
-					Button(
-						label=app[0],
-						name="light-suggestion",
-						h_align="start",
-						h_expand=True,
-						v_expand=True
-					)
-				)
-			if len(fitting) == SUGGESTIONS:
-				break
+		# start the process of adding slots with a lazy executor
+		# using this method makes the process of adding slots way more less
+		# resource expensive without blocking the main thread and resulting in a lock
+		self._arranger_handler = idle_add(
+			lambda *args: self.add_next_application(*args)
+			or (self.resize_viewport() if should_resize else False),
+			filtered_apps_iter,
+			pin=True,
+		)
 
-		self.light.children[1].children = fitting
+		return False
+
+	def add_next_application(self, apps_iter: Iterator[DesktopApp]):
+		if not (app := next(apps_iter, None)):
+			return False
+
+		self.viewport.add(self.bake_application_slot(app))
+		return True
+
+	def resize_viewport(self):
+		self.scrolled_window.set_min_content_width(
+			self.viewport.get_allocation().width  # type: ignore
+		)
+		return False
+
+	def bake_application_slot(self, app: DesktopApp, **kwargs) -> Button:
+		return Button(
+			child=Box(
+				orientation="h",
+				spacing=12,
+				name="light-suggestion",
+				children=[
+					Image(pixbuf=app.get_icon_pixbuf(), h_align="start", size=32),
+					Label(
+						label=app.display_name or "Unknown",
+						v_align="center",
+						h_align="center",
+					),
+				],
+			),
+			tooltip_text=app.description,
+			on_clicked=lambda *_: (app.launch(), self.toggle(None)),
+			**kwargs,
+		)
