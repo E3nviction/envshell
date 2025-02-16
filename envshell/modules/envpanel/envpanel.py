8fd641a050979aec6871b8985889b0206831de30
Enviction <schmidtjonas662@gmail.com>
2025-02-16 18:24:58 +0100

    added: more global menus

--- envshell/modules/envpanel/envpanel.py
+++ envshell/modules/envpanel/envpanel.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import subprocess
 import gi
 
 from fabric.core.service import Service, Signal, Property
@@ -31,7 +32,7 @@ from config.c import c
 
 def dropdown_option(self, label: str = "", keybind: str = "", on_click="echo \"EnvPanelDropdown Action\"", on_clicked=None):
 	def on_click_subthread(button):
-		self.toggle_dropdown(button)
+		envshell_service.current_dropdown = -1
 		if on_clicked: on_clicked(button)
 		else: exec_shell_command_async(on_click)
 	return Button(
@@ -126,6 +127,34 @@ class EnvPanel(Window):
 				self.global_title_menu_about
 			]
 		)
+		self.global_menu_file   = None
+		self.global_menu_edit   = None
+		self.global_menu_view   = EnvDropdown(0, 0,
+			dropdown_children=[
+				dropdown_option(self, "Enter Full Screen", on_click="hyprctl dispatch fullscreen"),
+			]
+		)
+		self.global_menu_go     = None
+		self.global_menu_window = EnvDropdown(0, 0,
+			dropdown_children=[
+				dropdown_option(self, "Zoom", on_clicked=lambda b: subprocess.run("bash ~/.config/scripts/zoomer.sh", shell=True)),
+				dropdown_option(self, "Move Window to Left", on_click="hyprctl dispatch movewindow l"),
+				dropdown_option(self, "Move Window to Right", on_click="hyprctl dispatch movewindow r"),
+				dropdown_option(self, "Cycle Through Windows", on_click="hyprctl dispatch cyclenext"),
+				dropdown_divider("---------------------"),
+				dropdown_option(self, "Float", on_click="hyprctl dispatch togglefloating"),
+				dropdown_option(self, "Center", on_click="hyprctl dispatch centerwindow"),
+				dropdown_option(self, "Group", on_click="hyprctl dispatch togglegroup"),
+				dropdown_option(self, "Pin", on_clicked=lambda b: subprocess.run("bash ~/.config/scripts/winpin.sh", shell=True)),
+			]
+		)
+		self.global_menu_help   = EnvDropdown(0, 0,
+			dropdown_children=[
+				dropdown_option(self, "EnvShell", on_clicked=lambda b: subprocess.run("xdg-open https://github.com/E3nviction/envshell", shell=True)),
+				dropdown_divider("---------------------"),
+				dropdown_option(self, "nixOS Help", on_clicked=lambda b: subprocess.run("xdg-open https://wiki.nixos.org/wiki/NixOS_Wiki", shell=True)),
+			]
+		)
 		envshell_service.connect("current-active-app-name-changed", lambda _, value: self.global_title_menu_about.set_property("label", f"About {value}"))
 		self.global_title = Button(
 			child=ActiveWindow(formatter=FormattedString("{ format_window('None', 'None') if win_title == '' and win_class == '' else format_window(win_title, win_class) }", format_window=self.format_window)),
@@ -134,10 +163,17 @@ class EnvPanel(Window):
 			on_clicked=self.global_title_dropdown.toggle_dropdown,
 		)
 
+		self.global_menu_button_file   = Button(label="File",   name="global-menu-button-file",   style_classes="button")
+		self.global_menu_button_edit   = Button(label="Edit",   name="global-menu-button-edit",   style_classes="button")
+		self.global_menu_button_view   = Button(label="View",   name="global-menu-button-view",   style_classes="button", on_clicked=lambda b: self.global_menu_view.toggle_dropdown(b, self.global_menu_button_view))
+		self.global_menu_button_go     = Button(label="Go",     name="global-menu-button-go",     style_classes="button")
+		self.global_menu_button_window = Button(label="Window", name="global-menu-button-window", style_classes="button", on_clicked=lambda b: self.global_menu_window.toggle_dropdown(b, self.global_menu_button_window))
+		self.global_menu_button_help   = Button(label="Help",   name="global-menu-button-help",   style_classes="button", on_clicked=lambda b: self.global_menu_help.toggle_dropdown(b, self.global_menu_button_help))
+
 		self.systray = SystemTray()
 
 		self.children = CenterBox(
-			start_children=[self.envsh_button, self.global_title],
+			start_children=[self.envsh_button, self.global_title, self.global_menu_button_file, self.global_menu_button_edit, self.global_menu_button_view, self.global_menu_button_go, self.global_menu_button_window, self.global_menu_button_help],
 			end_children=[self.power_button, self.wifi_button, self.search_button, self.control_center_button, self.date_time],
 		)
 
