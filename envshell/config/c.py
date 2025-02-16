f5621da3d574fe747593354d8d6ccadf00a1c03a
Enviction <schmidtjonas662@gmail.com>
2025-02-16 18:23:07 +0100

    update: made the config use toml and then programmatically write the config

--- envshell/config/c.py
+++ envshell/config/c.py
@@ -1,112 +1,128 @@
 from .conf import Config
+from fabric.utils import get_relative_path
+import tomllib
+import json
 
-c = Config()
-c.window_rule(is_wmclass="Hyprland", rule="autohide-panel")
-c.window_rule(is_wmclass="albert", rule="autohide-panel")
-c.window_rule(is_title="Albert", rule="autohide-panel")
-c.window_rule(is_wmclass="main.py", rule="autohide-panel")
-c.window_rule(is_wmclass="ulauncher", rule="autohide-panel")
 
+icon_list = []
 app_list = {
     "NotFOUND": "/run/current-system/sw/share/icons/WhiteSur-dark/apps/scalable/abrt.svg",
 }
+with open("./config/default_config.json", "r") as f:
+    default_config = json.load(f)
+try:
+    with open("./config/config.toml", "rb") as f:
+        config = tomllib.load(f)
+except:
+    print("Failed to load config.toml, using default config")
+    config = default_config
+json.dump(config, open("./config/latest_compiled_config.json", "w"), indent=4)
 
-c.window_rename(from_wmclass="code", to_wmclass="vscode")
-c.window_rename(from_wmclass="vesktop", to_wmclass="discord")
+c = Config()
 
+class SectionMissing():
+    def __init__(self, section):
+        return f"{section} section is missing from config.toml, using the default config"
+
+# Error Checking
+
+if config.get("Window", None) is None:
+    print(SectionMissing("Window"))
+    config["Window"] = default_config["Window"]
+
+if config.get("Dock", None) is None:
+    print(SectionMissing("Dock"))
+    config["Dock"] = default_config["Dock"]
+
+if config.get("Shell", None) is None:
+    print(SectionMissing("Shell"))
+    config["Shell"] = default_config["Shell"]
+
+if config.get("Shell").get("dock", None) is None:
+    print(SectionMissing("Shell.dock"))
+    config["Shell"]["dock"] = default_config["Shell"]["dock"]
+
+if config.get("Shell").get("panel", None) is None:
+    print(SectionMissing("Shell.panel"))
+    config["Shell"]["panel"] = default_config["Shell"]["panel"]
+
+if config.get("Shell").get("about", None) is None:
+    print(SectionMissing("Shell.about"))
+    config["Shell"]["about"] = default_config["Shell"]["about"]
+
+if config.get("Shell").get("env_menu", None) is None:
+    print(SectionMissing("Shell.env_menu"))
+    config["Shell"]["env_menu"] = default_config["Shell"]["env_menu"]
+
+if config.get("Icons", None) is None:
+    print(SectionMissing("Icons"))
+    config["Icons"] = default_config["Icons"]
+
+# Autohide
+for k, v in config["Window"].get("autohide", {}).get("class", {}).items():
+    c.window_rule(is_wmclass=k, rule="autohide-panel")
+for k, v in config["Window"].get("autohide", {}).get("title", {}).items():
+    c.window_rule(is_title=k, rule="autohide-panel")
+# Rename
+for k, v in config["Window"].get("rename", {}).get("class", {}).items():
+    c.window_rename(from_wmclass=k, to_wmclass=v)
+for k, v in config["Window"].get("rename", {}).get("title", {}).items():
+    c.window_rename(from_title=k, to_wmclass=v)
 # Ignore
-c.window_rule(is_wmclass="kitty-dropterm", rule="ignore")
-c.window_rule(is_wmclass="albert", rule="ignore")
-c.window_rule(is_wmclass="main.py", rule="ignore")
-c.window_rule(is_wmclass="ulauncher", rule="ignore")
-
+for k, v in config["Window"].get("ignore", {}).get("class", {}).items():
+    if v == True:
+        c.window_rule(is_wmclass=k, rule="ignore")
+for k, v in config["Window"].get("ignore", {}).get("title", {}).items():
+    if v == True:
+        c.window_rule(is_title=k, rule="ignore")
 # Dock
-c.pin_window(wmclass="org.gnome.Nautilus", command="nautilus")
-c.pin_window(wmclass="org.gnome.Settings", command="XDG_CURRENT_DESKTOP=GNOME gnome-control-center")
-c.pin_window(wmclass="org.gnome.Software", command="gnome-software")
-c.pin_window(wmclass="gnome-disks", command="gnome-disks")
-c.pin_window(wmclass="org.gnome.baobab", command="baobab")
-c.pin_window(wmclass="kitty", command="kitty")
-c.pin_window(wmclass="brave-browser", command="brave")
-c.pin_window(wmclass="code", command="code")
-c.pin_window(wmclass="org.gnome.Calculator", command="gnome-calculator")
-c.pin_window(wmclass="org.gnome.TextEditor", command="gnome-text-editor")
-c.pin_window(wmclass="org.gnome.Loupe", command="Image Viewer")
-c.pin_window(wmclass="org.gnome.clocks", command="gnome-clocks")
-c.pin_window(wmclass="krita", command="krita")
-c.pin_window(wmclass="spotify", command="spotify")
-c.pin_window(wmclass="vesktop", command="vesktop")
-c.pin_window(wmclass="org.keepassxc.KeePassXC", command="keepassxc")
-c.pin_window(wmclass="com.github.rafostar.Clapper", command="clapper")
-c.pin_window(wmclass="blender", command="blender")
-c.pin_window(wmclass="obsidian", command="obsidian")
-
+for k, v in config["Dock"].get("pinned", {}).items():
+    c.pin_window(wmclass=k, command=v)
 # Apps
-c.window_rule(from_wmclass="None", to_title="Hyprland")
-c.window_rule(from_wmclass="code", to_title="Visual Studio Code")
-c.window_rule(from_wmclass="brave-browser", to_title="Brave Browser")
-c.window_rule(from_wmclass="discord", to_title="Discord")
-c.window_rule(from_wmclass="vesktop", to_title="Vesktop")
-c.window_rule(from_wmclass="kitty", to_title="Kitty")
-c.window_rule(from_wmclass="org.keepassxc.KeePassXC", to_title="KeePassXC")
-c.window_rule(from_wmclass="org.gnome.Terminal", to_title="Terminal")
-c.window_rule(from_wmclass="org.gnome.Nautilus", to_title="Finder")
-c.window_rule(from_wmclass="org.gnome.NautilusPreviewer", to_title="Finder")
-c.window_rule(from_wmclass="org.gnome.Maps", to_title="Maps")
-c.window_rule(from_wmclass="org.gnome.Calculator", to_title="Calculator")
-c.window_rule(from_wmclass="org.gnome.Clocks", to_title="Clocks")
-c.window_rule(from_wmclass="brave-chatgpt.com__-Default", to_title="ChatGPT")
-c.window_rule(from_wmclass="obsidian", to_title="Obsidian")
-c.window_rule(from_wmclass="blender", to_title="Blender")
-c.window_rule(from_wmclass="spotify", to_title="Spotify")
-c.window_rule(from_wmclass="org.gnome.Software", to_title="App Store")
-c.window_rule(from_wmclass="org.gnome.Settings", to_title="Settings")
-c.window_rule(from_wmclass="gnome-disks", to_title="Disks")
-c.window_rule(from_wmclass="org.gnome.baobab", to_title="Disk Usage")
-c.window_rule(from_wmclass="org.gnome.TextEditor", to_title="Text Editor")
-c.window_rule(from_wmclass="org.gnome.Loupe", to_title="Image Viewer")
-c.window_rule(from_wmclass="org.gnome.clocks", to_title="Clocks")
-c.window_rule(from_wmclass="krita", to_title="Krita")
-c.window_rule(from_wmclass="com.github.rafostar.Clapper", to_title="Media Player")
-
+for k, v in config["Window"].get("translate", {}).get("class", {}).items():
+    c.window_rule(from_wmclass=k, to_title=v)
 
 # Workspace
-c.workspace_rule(9)
-
+for k, v in config["Workspace"]["ignore"]["id"].items():
+    if v == True:
+        c.workspace_rule(is_id=int(k), rule="ignore")
 
 # Shell
-c.shell_rule(rule="dock-width", value=782)
-c.shell_rule(rule="dock-height", value=64)
-c.shell_rule(rule="dock-position", value="bottom center")
-c.shell_rule(rule="dock-margin", value=(0, 0, 5, 0,))
-c.shell_rule(rule="dock-rounding", value="19.2px")
-c.shell_rule(rule="dock-orientation", value="horizontal")
+c.shell_rule(rule="dock-width", value=config["Shell"]["dock"].get("width", 782))
+c.shell_rule(rule="dock-height", value=config["Shell"]["dock"].get("height", 64))
+c.shell_rule(rule="dock-position", value=config["Shell"]["dock"].get("position", "bottom center"))
+c.shell_rule(rule="dock-margin", value=(
+    config["Shell"]["dock"].get("margin", (0, 0, 5, 0))[0],
+    config["Shell"]["dock"].get("margin", (0, 0, 5, 0))[1],
+    config["Shell"]["dock"].get("margin", (0, 0, 5, 0))[2],
+    config["Shell"]["dock"].get("margin", (0, 0, 5, 0))[3],
+))
+c.shell_rule(rule="dock-rounding", value=config["Shell"]["dock"].get("rounding", "19.2px"))
+c.shell_rule(rule="dock-orientation", value=config["Shell"]["dock"].get("orientation", "horizontal"))
 
-c.shell_rule(rule="panel-width", value=1920)
-c.shell_rule(rule="panel-height", value=24)
-c.shell_rule(rule="panel-position", value="top left right")
-c.shell_rule(rule="panel-margin", value=(0, 0, 0, 0))
-c.shell_rule(rule="panel-rounding", value="0")
-c.shell_rule(rule="panel-date-format", value="%a %b %d %H:%M")
-c.shell_rule(rule="panel-icon", value="")
+c.shell_rule(rule="panel-width", value=config["Shell"]["panel"].get("width", 1920))
+c.shell_rule(rule="panel-height", value=config["Shell"]["panel"].get("height", 24))
+c.shell_rule(rule="panel-position", value=config["Shell"]["panel"].get("position", "top left right"))
+c.shell_rule(rule="panel-margin", value=(
+    config["Shell"]["panel"].get("margin", (0, 0, 0, 0))[0],
+    config["Shell"]["panel"].get("margin", (0, 0, 0, 0))[1],
+    config["Shell"]["panel"].get("margin", (0, 0, 0, 0))[2],
+    config["Shell"]["panel"].get("margin", (0, 0, 0, 0))[3],
+))
+c.shell_rule(rule="panel-rounding", value=config["Shell"]["panel"].get("rounding", "0"))
+c.shell_rule(rule="panel-date-format", value=config["Shell"]["panel"].get("date-format", "%a %b %d %H:%M"))
+c.shell_rule(rule="panel-icon", value=config["Shell"]["panel"].get("icon", ""))
 
-c.shell_rule(rule="about-window-width", value=250)
-c.shell_rule(rule="about-window-height", value=355)
-c.shell_rule(rule="about-window-resizable", value=False)
-c.shell_rule(rule="about-computer-label", value="Env Shell")
-c.shell_rule(rule="about-computer-caption", value="ESH, 2024")
-c.shell_rule(rule="about-more-info", value="https://github.com/E3nviction/envshell")
+c.shell_rule(rule="about-window-width", value=config["Shell"]["about"].get("width", 250))
+c.shell_rule(rule="about-window-height", value=config["Shell"]["about"].get("height", 355))
+c.shell_rule(rule="about-window-resizable", value=config["Shell"]["about"].get("resizable", False))
+c.shell_rule(rule="about-computer-label", value=config["Shell"]["about"].get("computer-label", "Env Shell"))
+c.shell_rule(rule="about-computer-caption", value=config["Shell"]["about"].get("computer-caption", "ESH, 2024"))
+c.shell_rule(rule="about-more-info", value=config["Shell"]["about"].get("more-info", "https://github.com/E3nviction/envshell"))
 
-env_menu_option_labels = {
-    "About this PC": ["NOTEDITABLE"],
-    "System Settings...": ["code ~/.config/"],
-    "Nix Store...": ["xdg-open https://search.nixos.org/packages"],
-    "Force Quit App": ["hyprctl activewindow -j | jq -r .pid | xargs kill -9", "󰘶 󰘳 C"],
-    "Sleep": ["systemctl suspend", "󰘶 󰘳 M"],
-    "Restart...": ["systemctl restart", "󰘶 󰘳 M"],
-    "Shut Down...": ["shutdown now", "󰘶 󰘳 M"],
-    "Lock Screen": ["hyprlock", "󰘳 L"],
-}
+env_menu_option_labels = {}
+for num, se in config["Shell"].get("env_menu", {}).get("options", {}).items():
+    env_menu_option_labels[se.get("label", "")] = [se.get("on-click", ""), se.get("keybind", "")]
 
 i = 0
 for k, v in env_menu_option_labels.items():
@@ -118,5 +134,4 @@ for k, v in env_menu_option_labels.items():
 
 
 # Icons
-icon_list = []
-icon_dir = "/run/current-system/sw/share/icons/WhiteSur-dark/apps/scalable/"
+icon_dir = config["Icons"].get("directory", "/run/current-system/sw/share/icons/WhiteSur-dark/apps/scalable/")
