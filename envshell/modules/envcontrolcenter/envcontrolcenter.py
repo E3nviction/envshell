79f8f75668f618b4e2cf01722cd9a7d9bb172c11
Enviction <schmidtjonas662@gmail.com>
2025-02-22 18:25:30 +0100

    updated: control-center audio system

--- envshell/modules/envcontrolcenter/envcontrolcenter.py
+++ envshell/modules/envcontrolcenter/envcontrolcenter.py
@@ -77,17 +77,18 @@ class EnvControlCenter(Window):
 			**kwargs,
 		)
 
-		volume = envshell_service.sc("volume-changed", self.volume_changed, 100)
+		volume = 100
 		wlan = envshell_service.sc("wlan-changed", self.wlan_changed)
 		bluetooth = envshell_service.sc("bluetooth-changed", self.bluetooth_changed)
 		music = envshell_service.sc("music-changed", self.audio_changed)
 
 		audio_service.connect("changed", self.audio_changed)
+		audio_service.connect("changed", self.volume_changed)
 
 		self.wlan_label = Label(wlan, name="wifi-widget-label", h_align="start")
 		self.bluetooth_label = Label(bluetooth, name="bluetooth-widget-label", h_align="start")
 		self.volume_icon = Label(" ", name="volume-widget-icon", h_align="start")
-		self.volume_scale = Scale(value=volume, min_value=0, max_value=100, name="volume-widget-slider", size=30, h_expand=True)
+		self.volume_scale = Scale(value=volume, min_value=0, max_value=100, increments=(5, 5), name="volume-widget-slider", size=30, h_expand=True)
 		self.volume_scale.connect("change-value", self.set_volume)
 		self.music_label = Label(music, name="music-widget-label", h_align="start")
 
@@ -144,7 +145,7 @@ class EnvControlCenter(Window):
 		self.start_update_thread()
 
 	def set_volume(self, _, __, volume):
-		audio_service.speaker.volume = volume
+		audio_service.speaker.volume = round(volume)
 
 	def toggle_bluetooth(self, *_):
 		self.set_visible(False)
@@ -155,13 +156,12 @@ class EnvControlCenter(Window):
 			self.bluetooth_window.set_visible(False)
 		else:
 			self.set_visible(not self.is_visible())
-	def volume_changed(self, _, volume): GLib.idle_add(lambda: self.volume_scale.set_value(int(volume)))
+	def volume_changed(self, _, ):
+		GLib.idle_add(lambda: self.volume_scale.set_value(int(audio_service.speaker.volume)))
 	def wlan_changed(self, _, wlan): GLib.idle_add(lambda: self.wlan_label.set_property("label", wlan))
 	def bluetooth_changed(self, _, bluetooth): GLib.idle_add(lambda: self.bluetooth_label.set_property("label", bluetooth))
 	def audio_changed(self, *_):
-		if not audio_service.speaker:
-			return
-		envshell_service.volume = round(audio_service.speaker.volume)
+		pass
 	def start_update_thread(self):
 		def run():
 			try:
