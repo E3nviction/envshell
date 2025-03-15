bf17290a3721d94a2fbd9fec04a9d92edc312d52
Enviction <schmidtjonas662@gmail.com>
2025-03-15 14:32:45 +0100

    feat: add launch.nix

---
+++ launch.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+  buildInputs = [
+    pkgs.python312Packages.pip
+    pkgs.python312Packages.pygobject3
+    pkgs.python312Packages.psutil
+    pkgs.gtk4
+    pkgs.gtk3
+    pkgs.librsvg
+    pkgs.libdbusmenu-gtk3
+    pkgs.gdk-pixbuf
+    pkgs.cinnamon-desktop
+    pkgs.pulseaudio
+    pkgs.gtk-layer-shell
+    pkgs.gobject-introspection
+    pkgs.gnome-bluetooth
+  ];
+
+  shellHook = ''
+  	echo "Setting up linkers for python."
+    export PYTHONPATH="${pkgs.python312Packages.pygobject3}/lib/python3.12/site-packages"
+    echo "Activating python virtual env..."
+    source venv/bin/activate
+    cd envshell
+    clear
+    python main.py
+    exit
+  '';
+}
