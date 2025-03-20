b96d228fa19d80157f85b0b31e680d607d903261
Enviction <schmidtjonas662@gmail.com>
2025-03-20 17:42:38 +0100

    feat: add music player

--- envshell/modules/envcontrolcenter/envcontrolcenter.py
+++ envshell/modules/envcontrolcenter/envcontrolcenter.py
@@ -1,6 +1,7 @@
 import subprocess
 import threading
 import time
+import json
 
 
 from fabric.widgets.datetime import DateTime
@@ -12,10 +13,11 @@ from fabric.widgets.box import Box
 from fabric.widgets.eventbox import EventBox
 from fabric.widgets.scale import Scale
 from fabric.widgets.svg import Svg
+from fabric.widgets.stack import Stack
 from fabric.widgets.revealer import Revealer
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.widgets.scrolledwindow import ScrolledWindow
-from fabric.utils.helpers import exec_shell_command_async, get_relative_path
+from fabric.utils.helpers import exec_shell_command_async, get_relative_path, bulk_connect, exec_shell_command
 from fabric.audio import Audio
 from fabric.bluetooth import BluetoothClient, BluetoothDevice
 from gi.repository import GLib, Gtk
@@ -29,6 +31,11 @@ from utils.exml import exml
 from utils.functions import get_from_socket
 
 from widgets.popup_window import PopupWindow
+
+from styledwidgets.styled import styler, style_dict
+from styledwidgets.agents import margins, paddings, transitions, colors, shadows, borderradius, textsize
+from styledwidgets.types import px, rem
+from styledwidgets.color import alpha, hex
 
 from .bluetooth import BluetoothDeviceSlot, BluetoohConnections
 
@@ -88,111 +95,121 @@ Thanks to @slumberdemon for the following code.
 reference: https://github.com/SlumberDemon/dotfiles/blob/main/.config/fabric/widgets/sideleft/_player.py
 """
 class player(Box):
-    def __init__(self) -> None:
-        super().__init__(
-            name="left-player",
-            h_expand=True,
-            orientation="h",
-        )
+	def __init__(self, player=None) -> None:
+		super().__init__(
+			name="left-player",
+			h_expand=True,
+			orientation="h",
+		)
 
-        self.art = Button(name="left-player-art")
-        self.title = Label(
-            label="Nothing playing",
-            name="left-player-title",
-            justification="center",
-            ellipsization="end",
-            character_max_width=24,
-        )
-        self.artist = Label(
-            name="left-player-artist",
-            justification="center",
-            ellipsization="end",
-            character_max_width=20,
-        )
+		self.player = player
 
-        self.backward = Button(
-            child=Label(markup="<", name="left-player-icon")
-        )
-        self.forward = Button(
-            child=Label(markup=">", name="left-player-icon")
-        )
-        self.status = Label(
-            markup="󰐌", name="left-player-icon", style="font-size: 36px;"
-        )
-        self.play = Button(child=self.status)
+		self.art = Button(
+			name="left-player-art",
+			style=styler({
+				"#left-player-art": style_dict(
+					padding=px(5) + px(20),
+					background_size="cover",
+					background_color=alpha(colors.black, 0.2),
+					border_radius=px(6),
+					margin=px(8),
+				)
+			})
+		)
+		self.title = Label(
+			label="Nothing playing",
+			name="left-player-title",
+			justification="center",
+			ellipsization="end",
+			character_max_width=24,
+		)
+		self.artist = Label(
+			name="left-player-artist",
+			justification="center",
+			ellipsization="end",
+			character_max_width=20,
+		)
 
-        self.controls = CenterBox(
-            name="left-player-controls",
-            start_children=[self.backward],
-            center_children=[self.play],
-            end_children=[self.forward],
-            orientation="h",
-            h_expand=True,
-        )
-        self.info = Box(
-            name="left-player-info",
-            children=[self.title, self.artist, self.controls],
-            orientation="v",
-            v_align="center",
-            h_expand=True,
-        )
+		self.backward = Button(
+			child=Label(markup="", name="left-player-icon")
+		)
+		self.forward = Button(
+			child=Label(markup="", name="left-player-icon")
+		)
+		self.status = Label(
+			markup="", name="left-player-icon", style=styler(font_size=px(36))
+		)
+		self.play = Button(child=self.status)
 
-        self.details = Box(children=[self.info], h_expand=True, v_expand=True)
+		self.controls = CenterBox(
+			name="left-player-controls",
+			start_children=[self.backward],
+			center_children=[self.play],
+			end_children=[self.forward],
+			orientation="h",
+			h_expand=True,
+		)
+		self.info = Box(
+			name="left-player-info",
+			children=[self.title, self.artist, self.controls],
+			orientation="v",
+			v_align="center",
+			h_expand=True,
+		)
 
-        self.playerInfo = Fabricator(
-            poll_from='playerctl --follow metadata --format \'{"status": "{{status}}", "artUrl": "{{mpris:artUrl}}", "title": "{{ markup_escape(title) }}", "artist": "{{ markup_escape(artist) }}"}\'',
-            stream=True,
-            interval=1000,
-        )
+		self.details = Box(children=[self.info], h_expand=True, v_expand=True)
 
-        def extract_metadata(_, value):
-            if value:
-                data = json.loads(value)
+		self.playerInfo = Fabricator(
+			poll_from='playerctl '+("-p "+self.player if self.player else '')+' --follow metadata --format \'{"status": "{{status}}", "artUrl": "{{mpris:artUrl}}", "title": "{{ markup_escape(title) }}", "artist": "{{ markup_escape(artist) }}"}\'',
+			stream=True,
+			interval=1000,
+		)
 
-                self.art.set_style(f"background-image: url('{data['artUrl']}');")
-                self.title.set_label(data["title"])
-                self.artist.set_label(data["artist"])
-                self.status.set_markup(
-                    (
-                        "󰐌"
-                        if data["status"] == "Stopped"
-                        else (
-                            "H" if data["status"] == "Playing" else ">"
-                        )
-                    )
-                )
+		def extract_metadata(_, value):
+			if value:
+				data = json.loads(value)
 
-            else:
-                self.title.set_label("Nothing playing")
-                self.artist.set_label("")
-                self.status.set_markup("󰐌")
+				self.art.set_style(f"background-image: url('{data['artUrl']}');", append=True)
+				self.title.set_label(data["title"])
+				self.artist.set_label(data["artist"])
+				self.status.set_markup(
+					(
+						""
+						if data["status"] == "Stopped"
+						else (
+							"" if data["status"] == "Playing" else ""
+						)
+					)
+				)
+			else:
+				self.art.set_style("background-image: none;", append=True)
+				self.title.set_label("Nothing playing")
+				self.artist.set_label("")
+				self.status.set_markup("")
 
-        self.playerInfo.connect("changed", extract_metadata)
+		self.playerInfo.connect("changed", extract_metadata)
 
-        for connector in [self.backward, self.play, self.forward, self.art]:
-            bulk_connect(
-                connector,
-                {
-                    "enter-notify-event": lambda *args: self.set_cursor("pointer"),
-                    "leave-notify-event": lambda *args: self.set_cursor("default"),
-                    "button-press-event": self.on_button_press,
-                },
-            )
+		for connector in [self.backward, self.play, self.forward, self.art]:
+			bulk_connect(
+				connector,
+				{
+					"enter-notify-event": lambda *args: self.set_cursor("pointer"),
+					"leave-notify-event": lambda *args: self.set_cursor("default"),
+					"button-press-event": self.on_button_press,
+				},
+			)
 
-        self.add(self.art)
-        self.add(self.details)
+		self.add(self.art)
+		self.add(self.details)
 
-    def on_button_press(self, button: Button, event):
-        if event.button == 1 and event.type == 4:
-            if button == self.backward:
-                exec_shell_command("playerctl previous")
-            elif button == self.play:
-                exec_shell_command("playerctl play-pause")
-            elif button == self.forward:
-                exec_shell_command("playerctl next")
-        elif event.button == 1 and event.type == 5:
-            if button == self.art:
-                floating().toggle()  # fix not toggling # always shows, might even multiple?
+	def on_button_press(self, button: Button, event):
+		if event.button == 1 and event.type == 4:
+			if button == self.backward:
+				exec_shell_command("playerctl previous")
+			elif button == self.play:
+				exec_shell_command("playerctl play-pause")
+			elif button == self.forward:
+				exec_shell_command("playerctl next")
 
 class EnvControlCenter(Window):
 	"""Control Center for envshell"""
@@ -222,7 +239,11 @@ class EnvControlCenter(Window):
 		self.volume_scale = Scale(value=volume, min_value=0, max_value=100, increments=(5, 5), name="volume-widget-slider", size=30, h_expand=True)
 		self.volume_scale.connect("change-value", self.set_volume)
 
-		self.music_widget = player()
+		self.music_widget = Box(
+			name="music-widget",
+			children=[player()]
+		)
+
 
 		self.bluetooth_window = BluetoothWindow(parent=self)
 
