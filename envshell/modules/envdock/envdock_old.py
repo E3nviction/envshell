0f3cc19472b0a4aa9300c5bb99fa4789f3f144dc
Enviction <schmidtjonas662@gmail.com>
2025-03-20 15:57:43 +0100

    feat: make more similar to experimental envdock

--- envshell/modules/envdock/envdock_old.py
+++ envshell/modules/envdock/envdock_old.py
@@ -11,12 +11,17 @@ from fabric.widgets.svg import Svg
 from fabric.widgets.image import Image
 from fabric.widgets.label import Label
 from fabric.widgets.box import Box
+from fabric.widgets.eventbox import EventBox
 from fabric.widgets.centerbox import CenterBox
 from fabric.utils.helpers import exec_shell_command_async, get_relative_path
 from fabric.hyprland.widgets import get_hyprland_connection
 from fabric.widgets.wayland import WaylandWindow as Window
 from gi.repository import GLib
 import json
+
+from loguru import logger
+
+from gi.repository.GLib import idle_add
 
 
 from config.c import c, app_list
@@ -48,29 +53,10 @@ class EnvDock(Window):
 
 		self.icon_resolver = IconResolver()
 
-		self.connection = get_hyprland_connection()
-
-		if self.connection.ready:
-			self.refresh_apps()
-		else:
-			self.connection.connect("event::ready", lambda *_: self.refresh_apps())
-
-		for event in (
-			"activewindow",
-			"openwindow",
-			"closewindow",
-			"changefloatingmode",
-			"changeworkspace",
-			# add workspace events
-		):
-			self.connection.connect(f"event::{event}", lambda *_: self.refresh_apps())
-
-		if self.get_orientation() == "horizontal":
-			self.set_property("height-request", round(c.get_rule("Dock.size") * 64))
-		else:
-			self.set_property("width-request", round(c.get_rule("Dock.size") * 64))
-		self.set_property("margin", (0,0,0,0) if c.get_rule("Dock.mode") == "full" else (5,5,5,5))
-		self.set_property("anchor", self.get_pos())
+		if self.get_orientation() == "horizontal": idle_add(lambda: self.set_property("height-request", round(c.get_rule("Dock.size") * 64)))
+		else: idle_add(lambda: self.set_property("width-request", round(c.get_rule("Dock.size") * 64)))
+		idle_add(lambda: self.set_property("margin", (0,0,0,0) if c.get_rule("Dock.mode") == "full" else (5,5,5,5)))
+		idle_add(lambda: self.set_property("anchor", self.get_pos()))
 
 		envshell_service.connect(
 			"dock-apps-changed",
@@ -91,26 +77,41 @@ class EnvDock(Window):
 		)
 
 		self.connect("size-allocate", self.update_size)
-		envshell_service.dock_width = self.get_allocation().width
-		envshell_service.dock_height = self.get_allocation().height
-
+		self.update_size(None)
 
 		self.children = self.dock_box
 
-		self.connect("enter-notify-event", self.do_check_hide)
-		self.connect("leave-notify-event", self.do_check_hide)
+		self.connect("enter-notify-event", self._hide)
+		self.connect("leave-notify-event", self._hide)
 
 		if c.get_rule("Dock.autohide"):
 			envshell_service.connect(
 				"dock-hidden-changed",
-				self.do_check_hide,
+				self._hide,
 			)
+		self.connection = get_hyprland_connection()
+
+		if self.connection.ready:
+			self.refresh_apps()
+			GLib.timeout_add(500, self._hide)
+		else:
+			self.connection.connect("event::ready", lambda *_: self.refresh_apps())
+
+		for event in (
+			"activewindow",
+			"openwindow",
+			"closewindow",
+			"changefloatingmode",
+			"changeworkspace",
+		):
+			self.connection.connect(f"event::{event}", lambda *_: self.refresh_apps())
+		self.connection.connect("event::workspace", self._hide)
 
 	def show_dock(self, *_):
 		self.steal_input()
 		envshell_service.dock_hidden = False
 
-	def do_check_hover(self, *_):
+	def hide_dock(self, *_):
 		x, y = self.get_pointer()
 		allocation = self.get_allocation()
 		if y == 0:
@@ -121,22 +122,19 @@ class EnvDock(Window):
 		else:
 			envshell_service.dock_hidden = False
 
-	def do_check_hide(self, *_):
+	def _hide(self, *_):
+		if not c.get_rule("Dock.autohide"): return
 		if envshell_service.dock_hidden:
-			self.set_property("margin", (0,0,-(self.get_allocation().height),0))
+			idle_add(lambda: self.set_property("margin", (0,0,-(self.get_allocation().height),0)))
 			if self.get_orientation() == "vertical":
 				if c.get_rule("Dock.position") == "left":
-					self.set_property("margin", (0,0,0,-(self.get_allocation().width)))
+					idle_add(lambda: self.set_property("margin", (0,0,0,-(self.get_allocation().width))))
 				elif c.get_rule("Dock.position") == "right":
-					self.set_property("margin", (0,-(self.get_allocation().width),0,0))
-		else:
-			self.set_property("margin", (0,0,0,0) if c.get_rule("Dock.mode") == "full" else (5,5,5,5))
-
-
+					idle_add(lambda: self.set_property("margin", (0,-(self.get_allocation().width),0,0)))
+		else: idle_add(lambda: self.set_property("margin", (0,0,0,0) if c.get_rule("Dock.mode") == "full" else (5,5,5,5)))
 	def update_size(self, *_):
 		envshell_service.dock_width = self.get_allocation().width
 		envshell_service.dock_height = self.get_allocation().height
-
 	def get_pos(self):
 		pos = c.get_rule("Dock.position")
 		full = c.get_rule("Dock.mode") == "full"
@@ -149,7 +147,6 @@ class EnvDock(Window):
 			elif pos == "left": pos = "left center"
 			elif pos == "right": pos = "right center"
 		return pos
-
 	def get_orientation(self):
 		pos = c.get_rule("Dock.position")
 		orientation = "horizontal"
@@ -157,105 +154,99 @@ class EnvDock(Window):
 		elif pos == "left": orientation = "vertical"
 		elif pos == "right": orientation = "vertical"
 		return orientation
-
 	def focus_app(self, b):
-		os.system("hyprctl dispatch focuswindow address:" + b.get_name())
-
-	def launch_app(self, b):
+		exec_shell_command_async("hyprctl dispatch focuswindow address:" + b.get_name())
+	def launch_app(self, b: Button):
+		logger.info("Launching: " + b.get_name())
 		exec_shell_command_async(f"hyprctl dispatch exec {c.dock_pinned[b.get_name()]}")
-
-	def dock_apps_changed(self, _apps):
+	def dock_apps_changed(self, apps):
 		"""Update UI safely in the main thread."""
-		def dock_apps_changed_update():
+		def create_button(icon, name, active, running, on_click, tooltip):
+			button = Button(
+				child=icon,
+				name=name,
+				style_classes=("active" if active else "", "dock-app-button", "running" if running else ""),
+				h_align="center",
+				v_align="center",
+				on_clicked=on_click,
+				tooltip_text=tooltip,
+			)
+			app_button = None
+			if running:
+				app_button = Box(orientation="vertical", children=[
+					button,
+					Svg(svg_file=get_relative_path("../../assets/svgs/indicator.svg"), size=(6), name="dock-app-indicator", h_align="center", v_align="center"),
+				])
+			else:
+				app_button = Box(orientation="vertical", children=[button])
+			return app_button
+		def dock_apps_changed_update(apps):
 			self.dock_box.children = []
-			apps = _apps
-			total_apps = len(apps)
-			# We have to eval the string to get the list, because Signals don't work with lists. Atleast I haven't found a way
-			if apps[0] == "[" and apps[-1] == "]": apps: list = eval(apps)
 			pinned_apps = {}
-			apps_new = []
-			for i, app in enumerate(apps):
-				if app[0] not in c.dock_pinned:
-					apps_new.append(app)
-					continue
-				pinned_apps[app[0]] = [app[0], app[1], app[2], app[3], app[4], True]
-			apps = apps_new
+			temporary_apps = []
+			# We have to eval the string to get the list, because Signals don't work with lists. Atleast I haven't found a way
+			is_apps_list = apps[0] == "[" and apps[-1] == "]"
+			if is_apps_list:
+				apps: list = eval(apps)
+
+			# Here we sort out the pinned apps, that are running, and move them to running_pinned_apps
+			running_pinned_apps = [app for app in apps if app[0] in c.dock_pinned]
+			apps = [app for app in apps if app[0] not in c.dock_pinned]
+
+			# enrichen pinned apps with apps
 			for p in c.dock_pinned:
-				if p not in pinned_apps:
+				names_of_running_pinned_apps = [app[0] for app in running_pinned_apps]
+				current_app = [app for app in running_pinned_apps if app[0] == p]
+				if p not in names_of_running_pinned_apps:
 					pinned_apps[p] = [p, None, None, None, None, False]
+				else:
+					pinned_apps[p] = [p, current_app[0][1], current_app[0][2], current_app[0][3], current_app[0][4], True]
+
+			# Then we sort by config order
 			pinned_apps = dict(sorted(pinned_apps.items(), key=lambda item: list(c.dock_pinned.keys()).index(item[0])))
+
 			for app_ in pinned_apps:
 				app, pid, title, address, active, running = pinned_apps[app_]
-				svg     = Svg(svg_file=app_list["NotFOUND"], size=(round(c.get_rule("Dock.size") * 32)), name="dock-app-icon")
-				app_i = app
-				app_i = c.get_translation(wmclass=app)
-				icon_svg = self.icon_resolver.get_icon_pixbuf(app_i, round(c.get_rule("Dock.size") * 32))
-				svg     = Image(pixbuf=icon_svg, size=(round(c.get_rule("Dock.size") * 32)), name="dock-app-icon")
-				app = self.format_window(wmclass=app, title=title)
-				if running:
-					self.button = Button(
-						child=svg,
-						name=f"{address}",
-						style_classes=("active" if active else "", "dock-app-button", "running"),
-						h_align="center",
-						v_align="center",
-						on_clicked=self.focus_app,
-						tooltip_text=f"{app}",
-					)
-					app_button = Box(
-						orientation="vertical",
-						children=[
-							self.button,
-							Svg(svg_file=get_relative_path("../../assets/svgs/indicator.svg"), size=(6), name="dock-app-indicator", h_align="center", v_align="center"),
-						],
-					)
-				else:
-					app_button = Box(
-						orientation="vertical",
-						children=[
-							Button(
-								child=svg,
-								name=f"{pinned_apps[app_][0]}",
-								style_classes=("active" if active else "", "dock-app-button"),
-								h_align="center",
-								v_align="center",
-								on_clicked=self.launch_app,
-								tooltip_text=f"{app}",
-							),
-						],
-					)
+				svg = Image(
+					pixbuf=self.icon_resolver.get_icon_pixbuf(c.get_translation(wmclass=app), round(c.get_rule("Dock.size") * 32)),
+					size=(round(c.get_rule("Dock.size") * 32)),
+					name="dock-app-icon"
+				)
+				app_button = create_button(
+					svg,
+					address if address else app,
+					active,
+					running,
+					self.focus_app if running else self.launch_app,
+					self.format_window(wmclass=app, title=title)
+				)
 				self.dock_box.add(app_button)
-			if not len(apps) == 0:
+
+			if len(apps) != 0:
 				self.dock_box.add(Box(orientation="horizontal", name="dock-seperator", h_expand=True, v_expand=True))
+
 			for app, pid, title, address, active in apps:
 				app = c.get_translation(wmclass=app)
-				icon_svg = self.icon_resolver.get_icon_pixbuf(app, round(c.get_rule("Dock.size") * 32))
-				svg = Image(pixbuf=icon_svg, size=(round(c.get_rule("Dock.size") * 32)), name="dock-app-icon")
+				svg = Image(
+					pixbuf=self.icon_resolver.get_icon_pixbuf(app, round(c.get_rule("Dock.size") * 32)),
+					size=(round(c.get_rule("Dock.size") * 32)),
+					name="dock-app-icon"
+				)
 				app = self.format_window(wmclass=app, title=title)
-				app_button = Box(
-					orientation="vertical",
-					children=[
-						Button(
-							child=svg,
-							name=f"{address}",
-							style_classes=("active" if active else "", "dock-app-button", address),
-							h_align="center",
-							v_align="center",
-							on_clicked=self.focus_app,
-							tooltip_text=f"{app} ({f"{title[:c.get_rule('Dock.title.limit')]}..."})",
-						),
-						Svg(svg_file=get_relative_path("../../assets/svgs/indicator.svg"), size=(6), name="dock-app-indicator", h_align="center", v_align="center"),
-					],
+				app_button = create_button(
+					svg,
+					address,
+					active,
+					True,
+					self.focus_app,
+					f"{app} ({f"{title[:c.get_rule('Dock.title.limit')]}..."})"
 				)
 				self.dock_box.add(app_button)
 			self.dock_box.show_all()
 
-		GLib.idle_add(dock_apps_changed_update)
-
+		GLib.idle_add(dock_apps_changed_update, apps)
 	def format_window(self, title, wmclass):
-		name = app_name_class.format_app_name(title, wmclass, False)
-		return name
-
+		return app_name_class.format_app_name(title, wmclass, False)
 	def refresh_apps(self):
 		windows = self.fetch_clients()
 		open_apps = []
@@ -263,34 +254,28 @@ class EnvDock(Window):
 			pid = window["pid"]
 			title = window["title"]
 			address = window["address"]
-			app_name = window["initialClass"]
-			if app_name == "":
-				app_name = window["title"]
+			app_name = window["initialClass"] if window["initialClass"] != "" else window["title"]
 			if not (c.is_window_ignored(wmclass=str(app_name).lower()) or c.is_window_ignored(wmclass=str(app_name)) or c.is_workspace_ignored(id_=window["workspace"]["id"])):
-				try:
-					iaddress = self.get_focused_window()["address"]
-				except:
-					iaddress = None
-				open_apps.append([app_name, pid, str(title), address, iaddress == address])
+				focused_address = self.get_focused_window()
+				open_apps.append([app_name, pid, str(title), address, focused_address == address])
 		# sort alphabetically
 		open_apps = sorted(open_apps, key=lambda x: x[0])
 		envshell_service.dock_apps = str(open_apps)
-
 	def fetch_clients(self):
 		"""Retrieve open windows from Hyprland."""
+		try: return json.loads(self.connection.send_command("j/clients").reply.decode())
+		except json.JSONDecodeError: return []
+	def fetch_clients_current_workspace(self):
+		"""Retrieve open windows from Hyprland."""
 		try:
-			return json.loads(self.connection.send_command("j/clients").reply.decode())
-		except json.JSONDecodeError:
-			return []
-
+			clients   = json.loads(self.connection.send_command("j/clients").reply.decode())
+			workspace = json.loads(self.connection.send_command("j/activeworkspace").reply.decode())
+			return [client for client in clients if client["workspace"]["id"] == workspace["id"]]
+		except json.JSONDecodeError: return []
 	def get_focused_window(self):
 		"""Get the currently focused window's address."""
-		try:
-			return json.loads(
-				self.connection.send_command("j/activewindow").reply.decode()
-			).get("address", "")
-		except json.JSONDecodeError:
-			return ""
+		try: return json.loads(self.connection.send_command("j/activewindow").reply.decode()).get("address", "")
+		except json.JSONDecodeError:return ""
 
 class EnvDockHotspot(Window):
 	def __init__(self, dock, **kwargs):
