dcabf41e4f457cbb99b161f466caf07717abbde2
Enviction <schmidtjonas662@gmail.com>
2025-04-05 17:22:31 +0200

    feat: add ScreenFilter WIP

--- envshell/modules/envfilter/envfilter.py
+++ envshell/modules/envfilter/envfilter.py
@@ -15,9 +15,43 @@ from gi.repository import GLib # type: ignore
 from styledwidgets.styled import styler, style_dict
 from styledwidgets.agents import colors, borderradius, transitions, margins, paddings
 from styledwidgets.types import rem, px
+from styledwidgets.color import alpha
 
 from config.c import c
 from utils.functions import get_from_socket
+
+class BorderGlow(Window):
+	def __init__(self, **kwargs):
+		super().__init__(
+			layer="overlay",
+			anchor="top bottom left right",
+			exclusivity="none",
+			title="envshell-glow",
+			name="env-glow",
+			all_visible=True,
+			visible=True,
+			pass_through=True,
+			style=styler({
+				"default": style_dict(
+					background_color=alpha(colors.orange, 0),
+					box_shadow="inset 0 0 10px 1px #f00",
+					border_radius=px(12),
+					transition=transitions.normal,
+					padding=px(0),
+				)
+			}),
+			**kwargs,
+		)
+
+		self.set_size_request(1920, 1080)
+
+		if c.get_rule("Panel.mode") == "floating":
+			self.set_property("margin", (-(c.get_rule("Panel.height") + 10), 0, 0, 0))
+		elif c.get_rule("Panel.mode") == "normal":
+			self.set_property("margin", (-(c.get_rule("Panel.height")), 0, 0, 0))
+
+		self.children = []
+		self.show_all()
 
 class EnvFilter(Window):
 	"""Screen Filters for envshell, like Nightlight, Redshift, etc."""
@@ -26,21 +60,29 @@ class EnvFilter(Window):
 			layer="overlay",
 			anchor="top bottom left right",
 			exclusivity="none",
+			title="envshell-filter",
 			name="env-filter",
 			all_visible=True,
 			visible=True,
+			pass_through=True,
 			style=styler({
 				"default": style_dict(
-					background_color=colors.black,
+					background_color=alpha(colors.orange, 0),
+					border="1px solid #f00",
+					border_radius=px(12),
 					transition=transitions.normal,
-					border_radius=borderradius(0, 0, 0, 0),
-					padding=paddings(0, 0, 0, 0),
-					margin=margins(0, 0, 0, 0),
+					padding=px(0),
 				)
 			}),
-			margin=(-(c.get_rule("Panel.height")), 10, 10, 10),
 			**kwargs,
 		)
+
+		self.set_size_request(1920, 1080)
+
+		if c.get_rule("Panel.mode") == "floating":
+			self.set_property("margin", (-(c.get_rule("Panel.height") + 10), 0, 0, 0))
+		elif c.get_rule("Panel.mode") == "normal":
+			self.set_property("margin", (-(c.get_rule("Panel.height")), 0, 0, 0))
 
 		self.children = []
 		self.show_all()
